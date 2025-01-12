f59b6e170e047963317200e8a4e4127b37114bc3
Akseli Kolari <akzuu@pm.me>
2025-01-12 19:36:09 +0200

    Updating styles a bit

--- src/app/components/news-feed.tsx
+++ src/app/components/news-feed.tsx
@@ -22,20 +22,14 @@ export const NewsFeed: React.FC<NewsFeedProps> = ({ newsItems }) => {
           <div
             className="relative p-4 bg-white shadow-md rounded-lg overflow-hidden flex flex-col row-span-2"
             style={{
-              backgroundImage: topNewsItems[0].imageUrl
+              background: topNewsItems[0].imageUrl
                 ? `url(${topNewsItems[0].imageUrl})`
-                : "none",
+                : "#728276",
               backgroundSize: "cover",
               backgroundPosition: "center",
             }}
           >
-            <div
-              className={`relative ${
-                topNewsItems[0].imageUrl
-                  ? "bg-black bg-opacity-50 text-white"
-                  : ""
-              } p-4 rounded-lg flex-grow`}
-            >
+            <div className="relative bg-black bg-opacity-50 p-4 rounded-lg flex-grow">
               <h2 className="text-xl font-bold">{topNewsItems[0].title}</h2>
               {topNewsItems[0].description && (
                 <p className="mt-2">{topNewsItems[0].description}</p>
@@ -51,16 +45,12 @@ export const NewsFeed: React.FC<NewsFeedProps> = ({ newsItems }) => {
             key={index}
             className="relative p-4 bg-white shadow-md rounded-lg overflow-hidden flex flex-col"
             style={{
-              backgroundImage: item.imageUrl ? `url(${item.imageUrl})` : "none",
+              background: item.imageUrl ? `url(${item.imageUrl})` : "#728276",
               backgroundSize: "cover",
               backgroundPosition: "center",
             }}
           >
-            <div
-              className={`relative ${
-                item.imageUrl ? "bg-black bg-opacity-50 text-white" : ""
-              } p-4 rounded-lg flex-grow`}
-            >
+            <div className="relative bg-black bg-opacity-50 text-white p-4 rounded-lg flex-grow">
               <h2 className="text-xl font-bold">{item.title}</h2>
               {item.description && <p className="mt-2">{item.description}</p>}
               <p className="mt-2 text-sm">{format(item.date, "HH:mm")}</p>
