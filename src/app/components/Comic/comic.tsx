95eaa0ef98da5c58797f5b0f38621e45a03fb171
Akseli Kolari <akzuu@pm.me>
2025-01-13 20:59:54 +0200

    Make the comic larger since there is space

--- src/app/components/Comic/comic.tsx
+++ src/app/components/Comic/comic.tsx
@@ -31,8 +31,8 @@ const Comic: React.FC = () => {
           priority
           alt="Fok_It comic"
           src={comicUrl}
-          height={200}
-          width={567}
+          height={250}
+          width={709}
         />
 
         <div className="flex justify-between">
