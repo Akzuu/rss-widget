a708c9042e76503678c7a485720c5f111c833848
Akseli Kolari <akzuu@pm.me>
2025-02-26 19:34:09 +0200

    Use the new comic API

--- src/app/api/fokit/route.ts
+++ src/app/api/fokit/route.ts
@@ -1,46 +1,37 @@
-import scrapeIt from "scrape-it";
-
-type FokItScraper = {
-  cartoons: {
+type ComicResponse = {
+  id: number;
+  href: string;
+  displayDate: string;
+  title: string;
+  picture: {
+    id: number;
+    width: number;
+    height: number;
     url: string;
-  }[];
+    squareUrl: string;
+    photographer: string;
+  };
+  paidType: string;
+  category: string;
+  sectionTheme: string;
+  infoRowContent: {
+    timestamp: boolean;
+    paidIndicator: boolean;
+    saveButton: boolean;
+  };
+  tags: string[];
 };
 
 export async function GET() {
-  const fokitScrapingReq = await scrapeIt<FokItScraper>(
-    "https://www.hs.fi/sarjakuvat/fokit/",
-    {
-      cartoons: {
-        listItem: "img",
-        data: {
-          url: {
-            attr: "data-simple-src",
-          },
-        },
-      },
-    }
-  );
+  const comics = (await (
+    await fetch("https://www.hs.fi/api/laneitems/39221/list/normal/295")
+  ).json()) as ComicResponse[] | undefined;
 
-  if (fokitScrapingReq.status !== 200)
-    return Response.json("Internal Server Error", { status: 500 });
+  console.log(comics);
 
-  // By scraping it is only possible to get the image src for the small thumbnail
-  // iamge. That does not matter, since we can get the id for the image and thus
-  // determine the url for the whole image.
-  const urls = fokitScrapingReq.data.cartoons.map((e) => e.url);
+  if (comics === undefined || comics.length === 0) {
+    return Response.error();
+  }
 
-  const ids = urls
-    .map((url) => {
-      const match = url.match(/\/([a-f0-9]{32})\//);
-      return match ? match[1] : null;
-    })
-    .filter(Boolean);
-
-  if (ids.length === 0)
-    return Response.json("Internal Server Error", { status: 500 });
-
-  // At this moment, we are only interested about the latest comic
-  return Response.json(
-    `https://images.sanoma-sndp.fi/${ids[0]}/normal/1920.jpg.webp`
-  );
+  return Response.json(comics[0].picture.url);
 }
